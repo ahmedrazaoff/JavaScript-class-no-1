776d234401eaca3f77a6a578c3922497d69051b3
“AhmedRaza” <ahmedrazaoff@gmail.com>
2024-02-22 16:25:16 +0500

    java

---
+++ public/app.js
@@ -0,0 +1,188 @@
+// Chapter No 1
+// Q No 1
+
+alert("Error! Please Enter a valid password")
+
+
+// Q No 2
+
+alert("Welcome to JS Land \n Happy Coding!")
+
+//  Q No 3
+
+alert("Welcome to JS Land...")
+
+//  Q No 4
+
+alert("Happy Coding! \n  Prevent this page from creating additional dailogs.")
+
+// Q No 5
+
+alert("Hello... I can run JS thourgh web browser's console")
+
+
+
+// Chapter No 2
+
+// Q No 1
+
+var MyName = "Ahmed Raza" , age = 21 , Course = "Certifed Mobile Application Development"
+
+alert(MyName)
+alert(age)
+alert(Course)
+
+// Q No 3
+
+var name1 = "Pizza", name2 = "Pizz", name3 = "Piz", name4 = "Pi" , name5 = "P"
+
+alert(name1 +"\n"+name2 + "\n" + name3 + "\n" + name4 + "\n" + name5   )
+
+// Q No 4
+
+var email = " ahmedraza@gmail.com"
+
+alert("my email address is"+ email)
+
+// Q No 5
+
+var book = " A smarter way to learn JavaScript"
+
+alert("iam tring to learn from the Book" + book)
+
+// Q No 6
+
+var message = "Yah! I can Write HTML Content through Javascript"
+
+document.write(message)
+
+// Q No 7
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design)
+
+// Chapter No 3
+
+// Q No 1
+
+var age = 21
+
+alert("iam" + age + "years old")
+
+// Q No 2
+
+var visit = 14
+
+alert("you have visited this website " + visit + "times")
+
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+var birthYear = " 15 jan 1999"
+
+document.write("My Birth year is" + birthYear)
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+// Store visitor's information in variables
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+// Display order confirmation message in the browser
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+
+////////////////////// Chapter No 1///////////////////////////////
+
+//Q 1. Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// V.Password
+
+var FirstName = "Ahmed"
+var LastName = "Raza"
+var Email = "ahmedraza@gmail.com"
+var PhoneNo = 923170054678
+var Password = "Asddd123"
+
+alert(FirstName)
+alert(LastName)
+alert(Email)
+alert("+"+ PhoneNo)
+alert(Password)
+
+
+
+//Q 2. Correct this statement: alert "You're learning JavaScript"
+
+alert("You're learning JavaScript")
+
+// Q 3. Code an alert statement displaying any message you like.
+
+alert("Im learing JavaScript")
+
+
+// ////////////////////Chapter 2 (Variables for string)////////////////////////
+
+
+// Q 1. Declare any variable in the camel Case format.
+
+var cameCaseformat = "camel case"
+
+// Q 2. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+
+var Hello
+Hello = "Hello World" 
+
+
+// Q 3. Declare the variable teamName and Alert your Team name.
+
+var TeamName = "white Hat Coders"
+alert(TeamName)
+
+// Q 4. This statement has already been coded. var bestMan = Charlie; Assign the variable a new string.
+
+TeamName = "WHC"
+
+///////////////////////// Chapter 3 (Variables for numbers)///////////////////////////
+
+// Q 1. Declare a variable “caseQty”
+
+var caseQty
+
+// Q 2. Assign to the variable caseQty, which has already been declared, the value 144.
+
+
+caseQty = 144
+
+// Q 3. Rewrite the statement so the variable can be used in a math operation
+
+var num = 9; 
+
+// Q 4. Declare a variable and assign it the sum of 2 numbers
+var sum = 5 + 10;
+
+// Q 5. Determine the value of orderTotal
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+// Q 6. Declare a variable and assign it a number, then change its value by adding it together with another number
+var firstNumber = 7; 
+firstNumber += 3; 
+
+
